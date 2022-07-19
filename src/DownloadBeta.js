70dafd8a0161afef1339743a05d15037e3393490
Emmanuel <developersunesis@gmail.com>
2022-07-19 15:04:36 +0100

    Chore: Changed background color and

--- src/DownloadBeta.js
+++ src/DownloadBeta.js
@@ -1,12 +1,12 @@
 import windowsIcon from './res/windows-brands.svg'
 import appleIcon from './res/apple-brands.svg'
-import logo from "./res/logo.svg";
+import logo from "./res/dp-light.png";
 
 function DownloadBeta() {
-    return <div className={'App'}>
+    return <div className={'App BetaDownload'}>
         <center>
-            <img src={logo} style={{marginTop: '10vh'}} alt="logo"/>
-            <h2 style={{color: 'white', marginTop: '10px'}}>Use the download button below</h2>
+            <img src={logo} width={'100px'} alt="logo"/>
+            <h2 style={{marginTop: '5px', marginBottom: '10px'}}>Use the download button below</h2>
             <a href={'https://getace.app/beta-releases/Ace App-0.0.15-win-nsisx.exe'} className={'downloadBtn'}>
                 Download for Windows <img src={windowsIcon} alt={''}/>
             </a>
