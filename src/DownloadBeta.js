9a9c29c4cbfb35c7665f334e3a005516a0e14c7c
developersunesis <developersunesis@gmail.com>
2022-08-22 03:00:21 +0100

    Enabled v0.0.16 button

--- src/DownloadBeta.js
+++ src/DownloadBeta.js
@@ -7,11 +7,11 @@ function DownloadBeta() {
         <center>
             <img src={logo} width={'100px'} alt="logo"/>
             <h2 style={{marginTop: '5px', marginBottom: '10px'}}>Use the download button below</h2>
-            <a href={'https://getace.app/beta-releases/Ace App-0.0.15-win-nsisx.exe'} className={'downloadBtn'}>
-                Download for Windows <img src={windowsIcon} alt={''}/>
+            <a href={'https://getace.app/beta-releases/Ace App-0.0.16-win-nsisx.exe'} className={'downloadBtn'}>
+                Download for Windows (v0.0.16)<img src={windowsIcon} alt={''}/>
             </a>
-            <a href={'https://getace.app/beta-releases/Ace App-0.0.15-mac-x64.dmg'} className={'downloadBtn'}>
-                Download for Mac <img src={appleIcon} alt={''}/>
+            <a href={'https://getace.app/beta-releases/Ace App-0.0.16.dmg'} className={'downloadBtn'}>
+                Download for Mac (v0.0.16)<img src={appleIcon} alt={''}/>
             </a>
         </center>
     </div>
